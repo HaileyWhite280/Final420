fd61ec838773d971623ebf13474e0f0cf3bc7268
Meliemau <hwhite@student.neumont.edu>
2022-03-10 14:59:07 -0700

    Added some scripts

---
+++ Assets/Scripts/Gameplay/RollerPlayer.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RollerPlayer : MonoBehaviour, IDestructable
+{
+    [SerializeField] float maxForce = 5;
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] ForceMode forceMode;
+    [SerializeField] Transform viewTransform;
+
+    Rigidbody rb;
+    Vector3 force = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        //convert spaceDirection to cameraSpace
+        Quaternion viewSpace = Quaternion.AngleAxis(viewTransform.rotation.eulerAngles.y, Vector3.up);
+        direction = viewSpace * direction;
+
+        //worldSpace
+        force = direction * maxForce;
+
+        //viewSpace
+        //force = viewSpace * force;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        RollerGameManager.Instance.playerHealth = GetComponent<Health>().health;
+
+    }
+
+    //if consistently applied goes here
+    private void FixedUpdate()
+    {
+        //the new transform position
+        rb.AddForce(force, forceMode);
+
+    }
+
+    public void Destroyed()
+    {
+        RollerGameManager.Instance.OnPlayerDeath();
+    }
+}
---
+++ Assets/Scripts/SceneControl/Game.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Game : Singleton<Game>
+{
+	enum State
+	{
+		TITLE,
+		PLAYER_START,
+		GAME,
+		PLAYER_DEAD,
+		GAME_OVER
+	}
+
+	[SerializeField] ScreenFade screenFade;
+	[SerializeField] SceneLoader sceneLoader;
+	public GameData gameData;
+
+	State state = State.TITLE;
+	int highScore;
+
+	private void Start()
+	{
+		highScore = PlayerPrefs.GetInt("highscore", 0);
+		highScore++;
+		PlayerPrefs.SetInt("highscore", highScore);
+
+		InitScene();
+
+		SceneManager.activeSceneChanged += OnSceneWasLoaded;
+	}
+
+	void InitScene()
+	{
+		SceneDescriptor sceneDescriptor = FindObjectOfType<SceneDescriptor>();
+		if (sceneDescriptor != null)
+		{
+			Instantiate(sceneDescriptor.player, sceneDescriptor.playerSpawn.position, sceneDescriptor.playerSpawn.rotation);
+		}
+	}
+
+	private void Update()
+	{
+		switch (state)
+		{
+			case State.TITLE:
+				break;
+			case State.PLAYER_START:
+				break;
+			case State.GAME:
+				break;
+			case State.PLAYER_DEAD:
+				break;
+			case State.GAME_OVER:
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void onLoadScene(string sceneName)
+	{
+		sceneLoader.Load(sceneName);
+	}
+
+	public void OnPlayerDead()
+	{
+		gameData.intData["Lives"]--;
+
+		if (gameData.intData["Lives"] == 0)
+		{
+			onLoadScene("Main Menu");
+		}
+		else
+		{
+			onLoadScene(SceneManager.GetActiveScene().name);
+
+		}
+
+	}
+
+	public void OnSceneWasLoaded(Scene current, Scene next)
+	{
+		InitScene();
+	}
+}
